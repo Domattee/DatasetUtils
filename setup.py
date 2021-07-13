083dbde854ae9332950e5d27020f8a2dc8098c60
Domattee <domattee@yahoo.de>
2021-07-13 12:57:41 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wsdData',
+    version='0.1',
+    packages=['wsdData'],
+    url='',
+    license='',
+    author='Dominik Mattern',
+    author_email='domattee@yahoo.de',
+    description='Dataset classes for WSD tasks and pipelines for the verb paper'
+)
